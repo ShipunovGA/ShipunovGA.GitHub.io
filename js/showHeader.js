1328f496a6df31c13e7957f1549bf3d7eb2e14d3
GrindelfP <grindelf.perlomutrovij@gmail.com>
2022-11-14 07:28:10 +0300

    create separate header

---
+++ js/showHeader.js
@@ -0,0 +1,45 @@
+// const hder = document.querySelector('.header');
+// fetch('../pagesUtility/header.html')
+// .then(res=>res.text)
+// .then(data=>{
+//     hder.innerHTML=data
+// })
+
+document.write(`
+
+    <header class="header" id="headInc">
+        <div>
+            <h1>Jotunheim library</h1>
+            <h3 class="h3h">The one under the mountain.</h3>
+        </div>
+
+        <div class="logo">
+            <a class="ah" href="index.html">
+                <img src="src/logo.jpeg" alt="Logotype" height="129" width="129">
+            </a>
+        </div>
+
+        <nav class="navigation">
+            <ul>
+                <li>
+                    <a class="ah" href="index.html">About me</a>
+                </li>
+                <li>
+                    <a class="ah" href="index.html#comps">My skills</a>
+                </li>
+                <li>
+                    <a class="ah" href="pages/summary.html">My summary</a>
+                </li>
+                <li>
+                    <a class="ah" href="pages/portfolio.html">Portfolio</a>
+                </li>
+                <li>
+                    <a class="ah" href="pages/JS.html">JS task</a>
+                </li>
+                <li>
+                    <a class="ah" href="pages/contacts.html">Contacts</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+`);
